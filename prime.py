75fad4850a780dc7ac8c0c05e52bd6082fe16996
IanRiceDev <the3bears@centurylink.net>
2016-10-20 12:19:03 -0400

    fixed?

--- prime.py
+++ prime.py
@@ -3,6 +3,7 @@ numberinput = input("Type any number to see if it is a prime number: ")
 message = ""
 basePrimes = [2,3,5,7]
 notPrimes = [0,1]
+listcount = 0
 
 while True:
 
@@ -16,20 +17,23 @@ while True:
         m2 = baseNum
         break
 
+while listcount < 5:
+    for prime in basePrimes:
 
-for prime in basePrimes:
+        if m2 % prime == 0:
+            message = numberinput + " is not a prime number."
+            break
 
-    if m2 % 2 == 0:
-        message = numberinput + " is not a prime number."
-        break
+        #if m2 % 3 == 0:
+            #message = numberinput + " is not a prime number."
+            #break
+        if listcount <= 4:
+            listcount += 1
+            continue
 
-    if m2 % 3 == 0:
-        message = numberinput + " is not a prime number."
-        break
-
-    else:
-        message = numberinput + " is a prime number."
-        break
+        if listcount == 5:
+                message = numberinput + " is a prime number."
+                break
 
 if m2 in basePrimes:
     message = numberinput+" is a prime number."
@@ -42,4 +46,4 @@ if m2 in notPrimes:
 print("\n")
 print(message)
 print("\n")
-input("Press the enter key to exit")
+#input("Press the enter key to exit")
