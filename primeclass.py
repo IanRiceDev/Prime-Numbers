e6b2e336a7e0fafe072cf51458f81daa7cff4c07
IanRiceDev <the3bears@centurylink.net>
2016-12-15 13:53:19 -0500

    update

--- primeclass.py
+++ primeclass.py
@@ -1,32 +1,32 @@
 # defines this file as a class
 def check_if_prime(numberinput):
 
-    # defines vars
+    # defines variable
     numbertest = str(numberinput)
     message = ""
     basePrimes = [2,3,5,7]
     notPrimes = [0,1]
 
-    # loop where the work of the class takes place
+    # Loop where the work of the class takes place
     while True:
-        # checks if the in put is a number before math of the class
+        # Checks if the in put is a number before math of the class
         if numbertest.isdigit() == False:
             print("That is not a number")
             break
-        # defines vars in the scope of the loop
+        # Defines variable in the scope of the loop
         if True:
             numberoutput = int(numberinput)
             baseNum = numberoutput
             basePrime = baseNum
-        # checks if number is a part of base primes
+        # Checks if number is a part of base primes
         if basePrime in basePrimes:
             message = numbertest+" is a prime number number."
             break
-        # checks if number is a part of numbers that can not be prime
+        # Checks if number is a part of numbers that can not be prime
         if basePrime in notPrimes:
             message = numbertest+" is not a prime number."
             break
-        # uses modelo oprater to test remander of devishon for primes
+        # Uses models operator to test remainder of division for primes
         if basePrime % 2 == 0:
             message = numbertest+" is not a prime number."
             break
@@ -39,9 +39,9 @@ def check_if_prime(numberinput):
         if basePrime % 7 == 0:
             message = numbertest+" is not a prime number."
             break
-        # else must be prime
+        # Else must be prime
         else:
             message = numbertest+" is a prime number."
             break
-    # returns var message
+    # Returns variable message
     return message
